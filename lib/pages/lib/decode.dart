fc81cfbefd4480ce4c231ca71271ee4e197d2087
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-08-14 19:54:05 +0500

    capitalization for version 1

--- lib/pages/lib/decode.dart
+++ lib/pages/lib/decode.dart
@@ -75,12 +75,15 @@ String decode(String masterInput, int version, bool caseSensitive) {
       if (caseSensitive) {
         if (version == 1) {
           if (nAcc == 717) {
-            token = cipher(nAcc, version); // capitalize next letter
+            _cL = true; // capitalize next letter
+            token = null; acc = ""; accL = 0;
+            // setting token to null skips adding to out and doesnt reset _cL
           } else if (nAcc == 727 || _cW) {
-            token = cipher(nAcc, version); // still capital, but by word
+            _cW = true; // still capital, but by word
+            token = null; acc = ""; accL = 0;
           } else if (nAcc == 737) {
-            token = ""; // this is a repeated 737, we ignore it...
-            // quite a rare case
+            token = null; // this is a repeated 737, we ignore it... quite a rare case
+            acc = ""; accL = 0;
           } else {
             token = _cS ? cipher(nAcc, version) : cipher(nAcc, version)?.toLowerCase();
           }
@@ -92,9 +95,10 @@ String decode(String masterInput, int version, bool caseSensitive) {
       }
 
       if (token != null) {
-        out.write(token);
+        out.write(caseSensitive ? (_cW ? token.toUpperCase() : (_cL ? token.toUpperCase() : token.toLowerCase())) : token);
         acc = "";
         accL = 0;
+        _cL = false;
       }
     }
 
